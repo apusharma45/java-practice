2deec8cf01f360ff633e37109db61e6ec85abc6a
apusharma45 <arapusharma45@gmail.com>
2025-09-20 22:21:19 +0600

    first commit

---
+++ Student.java
@@ -0,0 +1,46 @@
+public class Student {
+    private String name;
+    private int age;
+    private String id;
+
+    // Constructor
+    public Student(String name, int age, String id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for age
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter and Setter for id
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    // Optional: toString method for easy printing
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
