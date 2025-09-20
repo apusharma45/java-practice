cc43f48ad5acef27431959962b41bd61dfc90905
apusharma45 <arapusharma45@gmail.com>
2025-09-20 22:25:08 +0600

    calculator added to feature-calculator

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+public class Calculator {
+
+    // Addition
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Division (throws ArithmeticException if b is 0)
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    // Optional: main method to test manually
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("5 + 3 = " + calc.add(5, 3));
+        System.out.println("5 - 3 = " + calc.subtract(5, 3));
+        System.out.println("5 * 3 = " + calc.multiply(5, 3));
+        System.out.println("6 / 2 = " + calc.divide(6, 2));
+    }
+}
